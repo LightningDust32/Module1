8b024491386fa2dea5bd99b71d0871395c31cae9
LightningDust32 <superiorhunter09@gmail.com>
2026-09-25 18:50:39 +0100

    Made the base player, with input hooks and the time handling

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,127 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform cameraTransform;
+
+    [Header("Elephant Stats")]
+    [SerializeField] private float elephantHunger = 100f;
+    [SerializeField] private float elephantThirst = 100f;
+    [SerializeField] private float elephantFatigue = 100f;
+    [SerializeField] private float elephantTemperature = 100f;
+
+    [Header("Elephant Maximums")]
+    [SerializeField] private float elephantMaxHunger = 100f;
+    [SerializeField] private float elephantMaxThirst = 100f;
+    [SerializeField] private float elephantMaxFatigue = 100f;
+    [SerializeField] private float elephantMaxTemperature = 100f;
+
+    [Header("Handler Stats")]
+    [SerializeField] private float handlerHunger = 100f;
+    [SerializeField] private float handlerThirst = 100f;
+    [SerializeField] private float handlerFatigue = 100f;
+
+    [Header("Handler Maximums")]
+    [SerializeField] private float handlerMaxHunger = 75f;
+    [SerializeField] private float handlerMaxThirst = 75f;
+    [SerializeField] private float handlerMaxFatigue = 75f;
+
+    [Header("Time")]
+    [SerializeField] private int currentHour = 6;
+    [SerializeField] private int currentMinute = 0;
+
+    [SerializeField] private int sunriseHour = 6;
+    [SerializeField] private int sunsetHour = 18;
+
+    // Input
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+
+    private InputSystem_Actions inputActions;
+
+    private CharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        inputActions.Dispose();
+    }
+
+    // Time
+    public void AdvanceTime(int minutes)
+    {
+        currentMinute += minutes;
+
+        while (currentMinute >= 60)
+        {
+            currentMinute -= 60;
+            currentHour++;
+        }
+
+        Debug.Log($"Time: {GetTimeString()}");
+    }
+
+    public string GetTimeString()
+    {
+        return $"{currentHour:00}:{currentMinute:00}";
+    }
+
+    public bool IsDaylight()
+    {
+        return currentHour >= sunriseHour &&
+               currentHour < sunsetHour;
+    }
+
+
+    //Input Stubs
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        // Stub
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        // Stub
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // Stub
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        // Stub
+    }
+}
